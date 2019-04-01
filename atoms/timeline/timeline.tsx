cefd50cc54279036871ba78b7fa7d7fd145bca51
tbatie <tracy.batie@connexta.com>
2019-04-01 12:38:51 -0700

    Finished moving everything to react

--- atoms/timeline/timeline.tsx
+++ atoms/timeline/timeline.tsx
@@ -379,22 +379,188 @@ type State = {
   points: any[]
 }
 
-const Circles = ({ points, onClick, margin }: any) => {
+const Circles = ({ points, onClick, onHover, margin }: any) => {
   //.on('mousemove', displayTooltip(p => onHover([p])))
   //.on('mouseout', hideTooltip)
-  console.log(points)
+
+  const createClusterPoint = (points: any[]): ClusterPoint => {
+    const pointCxs: number[] = points.map(p => p.cx)
+    const max = Math.max(...pointCxs)
+    const min = Math.min(...pointCxs)
+    const radius = (max - min + 2 * POINT_RADIUS) / 2
+    const cx = min - POINT_RADIUS + radius
+
+    // All points are on the y axis, no need to take average
+    const cy = points[0].cy
+    return { cx, cy, radius, points: points.map(p => p.point) }
+  }
+
+  // //results are clusters of indices relating to array of coordiantes passed in
+  var clusters = dbscan.run(
+    points.map((p: any) => [p.cx, p.cy]),
+    CLUSTER_NEIGHBOR_RADIUS,
+    POINTS_TO_FORM_CLUSTER
+  ) as number[][]
+
+  const newClusterPoints = clusters
+    .filter(cluster => cluster.length > 1)
+    .map(cluster => cluster.map(i => points[i]))
+    .map(createClusterPoint)
+
+  // pointsContainer
+  //     .selectAll('clust')
+  //     .data(newClusterPoints)
+  //     .enter()
+  //     .append('circle')
+  //     .attr('class', 'cluster')
+  //     .attr('fill', 'grey')
+  //     .attr('opacity', '0.25')
+  //     .attr('cx', c => c.cx + 'px')
+  //     .attr('cy', c => c.cy + 'px')
+  //     .attr('r', c => c.radius)
+  //     .attr('transform', 'translate(' + -margin.left + ',0)')
+  //     .on('click', cluster => onClick(cluster.points))
+  //     .on('mousemove', displayTooltip(cluster => onHover(cluster.points)))
+  //     .on('mouseout', hideTooltip)
+
+  //   pointsContainer
+  //     .selectAll('clust')
+  //     .data(newClusterPoints)
+  //     .enter()
+  //     .append('rect')
+  //     .attr('fill', 'black')
+  //     .attr('class', 'cluster-background')
+  //     .attr(
+  //       'x',
+  //       c => c.cx - 5 - (c.points.length.toString().length / 2) * 10 + 'px'
+  //     )
+  //     .attr('y', c => 30 + c.cy - 16 + 'px')
+  //     .attr('width', c => c.points.length.toString().length * 10 + 10)
+  //     .attr('height', c => 20)
+  //     .attr('transform', 'translate(' + -margin.left + ',0)')
+  //     .on('click', cluster => onClick(cluster.points))
+  //     .on('mousemove', displayTooltip(cluster => onHover(cluster.points)))
+  //     .on('mouseout', hideTooltip)
+
+  //   pointsContainer
+  //     .selectAll('clust')
+  //     .data(newClusterPoints)
+  //     .enter()
+  //     .append('rect')
+  //     .attr('fill', 'black')
+  //     .attr('class', 'cluster-background')
+  //     .attr('x', c => c.cx - 1 + 'px')
+  //     .attr('y', c => 15 + c.cy - 16 + 'px')
+  //     .attr('width', 2)
+  //     .attr('height', 15)
+  //     .attr('transform', 'translate(' + -margin.left + ',0)')
+  //     .on('click', cluster => onClick(cluster.points))
+  //     .on('mousemove', displayTooltip(cluster => onHover(cluster.points)))
+  //     .on('mouseout', hideTooltip)
+
+  //   pointsContainer
+  //     .selectAll('clust')
+  //     .data(newClusterPoints)
+  //     .enter()
+  //     .append('text')
+  //     .attr('fill', 'white')
+  //     .attr('class', 'cluster-text')
+  //     .attr(
+  //       'x',
+  //       c => c.cx - (c.points.length.toString().length * 10) / 2 + 'px'
+  //     )
+  //     .attr('y', c => 30 + c.cy + 'px')
+  //     .attr('font-size', 20)
+  //     .attr('transform', 'translate(' + -margin.left + ',0)')
+  //     .text(c => c.points.length)
+  //     .on('click', cluster => onClick(cluster.points))
+  //     .on('mousemove', displayTooltip(cluster => onHover(cluster.points)))
+  //     .on('mouseout', hideTooltip)
+  const handleHover = (points: any) => (e: any) => {
+    onHover({ x: e.pageX, y: e.pageY, points })
+  }
   return (
     <>
-      {points.map((point: any) => {
+      {newClusterPoints.map((cluster: ClusterPoint, i: number) => {
+        const rectX =
+          cluster.cx -
+          5 -
+          (cluster.points.length.toString().length / 2) * 10 +
+          'px'
+        const rectY = 30 + cluster.cy - 16 + 'px'
+        const rectWidth = cluster.points.length.toString().length * 10 + 10
+        const rectHeight = 20
+
+        const textX =
+          cluster.cx - (cluster.points.length.toString().length * 10) / 2 + 'px'
+        const textY = 30 + cluster.cy + 'px'
+        return (
+          <React.Fragment key={i}>
+            <circle
+              className="cluster"
+              fill="grey"
+              opacity="0.25"
+              cx={cluster.cx}
+              cy={cluster.cy}
+              r={cluster.radius}
+              transform={'translate(' + -margin.left + ',0)'}
+              onClick={() => onClick(cluster.points)}
+              onMouseMove={handleHover(cluster.points)}
+              onMouseOut={() => onHover([])}
+            />
+            <rect
+              className="cluster-background"
+              fill="black"
+              x={rectX}
+              y={rectY}
+              width={rectWidth}
+              height={rectHeight}
+              transform={'translate(' + -margin.left + ',0)'}
+              onMouseMove={handleHover(cluster.points)}
+              onMouseOut={() => onHover([])}
+              onClick={() => onClick(cluster.points)}
+            />
+            <rect
+              className="cluster-background"
+              fill="black"
+              x={cluster.cx - 1 + 'px'}
+              y={15 + cluster.cy - 16 + 'px'}
+              width={2}
+              height={15}
+              transform={'translate(' + -margin.left + ',0)'}
+              onMouseMove={handleHover(cluster.points)}
+              onMouseOut={() => onHover([])}
+              onClick={() => onClick(cluster.points)}
+            />
+            <text
+              fill="white"
+              className="cluster-text"
+              x={textX}
+              y={textY}
+              fontSize="20"
+              transform={'translate(' + -margin.left + ',0)'}
+              onMouseMove={handleHover(cluster.points)}
+              onMouseOut={() => onHover([])}
+              onClick={() => onClick(cluster.points)}
+            >
+              {cluster.points.length}
+            </text>
+          </React.Fragment>
+        )
+      })}
+      {points.map((point: any, i: number) => {
         return (
           <circle
+            key={i}
             fill="gray"
             opacity="0.25"
             cx={point.cx}
             cy={point.cy}
             r={POINT_RADIUS}
             transform={'translate(' + -margin.left + ',0)'}
-            //onClick={() => onClick([point])}
+            onMouseMove={handleHover([point.point])}
+            onMouseOut={() => onHover([])}
+            onClick={() => onClick([point.point])}
           />
         )
       })}
@@ -402,7 +568,9 @@ const Circles = ({ points, onClick, margin }: any) => {
   )
 }
 
-class Timeline extends React.Component<Props, State> {
+const margin = { top: 20, right: 20, bottom: 30, left: 40 }
+
+class Timeline extends React.Component<Props, any> {
   d3Ref = React.createRef()
   view: any = null
   constructor(props: Props) {
@@ -411,18 +579,22 @@ class Timeline extends React.Component<Props, State> {
       width: 0,
       height: 0,
       points: [],
+      tooltip: undefined,
+    }
+  }
+  onHover = (e: any) => {
+    const { x, y, points = [] } = e
+    if (points.length === 0) {
+      this.setState({ tooltip: undefined })
+    } else {
+      this.setState({ tooltip: { x, y } })
     }
   }
   draw = () => {
-    /*this.view = draw(
-      this.props.value,
-      this.d3Ref.current,
-      this.props.onHover,
-      this.props.onClick
-    )*/
+    this.setState({})
   }
   onZoom = () => {
-    /*const timelineContainer = d3.select('.timeline')
+    const timelineContainer = d3.select('.timeline')
     let t = d3.zoomTransform(timelineContainer.node() as any)
 
     // TODO - No idea why k ends up being -1, this is a hacky fix :(
@@ -442,23 +614,18 @@ class Timeline extends React.Component<Props, State> {
     const yAxis = d3.axisLeft(newYScale)
 
     d3.select('.axis--x').call(xAxis.scale(newXScale))
-    d3.select('.axis--y').call(yAxis.scale(newYScale))*/
-    /*points
-      .attr('cx', d => {
-        const cx = newXScale(d.date)
-        if (isNaN(cx)) {
-          throw 'cx of point calculated as NaN when rescaling axises'
-        }
-        return margin.left + cx
-      })
-      .attr('cy', d => {
-        const cy = newYScale(Y_VALUE)
-        if (isNaN(cy)) {
-          throw 'cy of point calculated as NaN when rescaling axises'
-        }
-        return cy
-      })*/
+    d3.select('.axis--y').call(yAxis.scale(newYScale))
+
+    const points = this.props.value.map(point => {
+      const cx = newXScale(point.date)
+      const cy = newYScale(Y_VALUE)
+      return { cx, cy, point }
+    })
+
+    this.setState({ points })
   }
+  zoomIn() {}
+  zoomOut() {}
   componentDidMount() {
     window.addEventListener('resize', this.draw)
     if (this.d3Ref.current !== null) {
@@ -499,23 +666,40 @@ class Timeline extends React.Component<Props, State> {
       var zoomMin = 1
       var zoomMax = (xAxisMax.getTime() - xAxisMin.getTime()) / (1000 * 60)
 
-      // const zoom = d3
-      //   .zoom()
-      //   .scaleExtent([zoomMin, zoomMax])
-      //   .translateExtent([[0, 0], [width, height]])
-      //   .extent([[0, 0], [width, height]])
-      //   .on('zoom', this.onZoom)
+      const zoom = d3
+        .zoom()
+        .scaleExtent([zoomMin, zoomMax])
+        .translateExtent([[0, 0], [width, height]])
+        .extent([[0, 0], [width, height]])
+        .on('zoom', this.onZoom)
 
-      // d3.select('.timeline')
-      //   .call(zoom)
-      //   .transition()
-      //   .duration(1500)
-      //   .call(
-      //     zoom.transform as any,
-      //     d3.zoomIdentity
-      //       .scale(width / (x(xAxisMin) - x(xAxisMax)))
-      //       .translate(-x(xAxisMin), 0)
-      //   )
+      const timelineContainer = d3.select('.timeline') as d3.Selection<
+        SVGSVGElement,
+        {},
+        HTMLElement,
+        any
+      >
+
+      // TODO: try and use onZoom here instead
+      this.zoomIn = () => {
+        zoom.scaleBy(timelineContainer.transition().duration(500), 2)
+      }
+
+      // TODO: try and use onZoom here instead
+      this.zoomOut = () => {
+        zoom.scaleBy(timelineContainer.transition().duration(500), 0.2)
+      }
+
+      d3.select('.timeline')
+        .call(zoom)
+        .transition()
+        .duration(1500)
+        .call(
+          zoom.transform as any,
+          d3.zoomIdentity
+            .scale(width / (x(xAxisMin) - x(xAxisMax)))
+            .translate(-x(xAxisMin), 0)
+        )
     }
     this.draw()
   }
@@ -527,25 +711,34 @@ class Timeline extends React.Component<Props, State> {
     window.removeEventListener('resize', this.draw)
   }
   render() {
-    const margin = { top: 20, right: 20, bottom: 30, left: 40 }
     const width = this.state.width - margin.left - margin.right
     const height = SVG_HEIGHT - margin.top - margin.bottom
 
     return (
       <Root style={this.props.style} className={this.props.className}>
         <ZoomArea>
-          <Button onClick={() => this.view.zoomIn()} emphasis="medium">
+          <Button onClick={this.zoomIn} emphasis="medium">
             +
           </Button>
-          <Button onClick={() => this.view.zoomOut()} emphasis="medium">
+          <Button onClick={this.zoomOut} emphasis="medium">
             -
           </Button>
         </ZoomArea>
         <div style={{ flexDirection: 'column' }}>
-          <div
-            className="tooltip"
-            style={{ position: 'absolute', pointerEvent: 'none', opacity: 0 }}
-          />
+          {(this as any).state.tooltip !== undefined ? (
+            <div
+              className="tooltip"
+              style={{
+                opacity: 0.9,
+                position: 'fixed',
+                left: (this as any).state.tooltip.x,
+                top: (this as any).state.tooltip.y + 50,
+                pointerEvent: 'none',
+              }}
+            >
+              hello
+            </div>
+          ) : null}
           <svg
             ref={this.d3Ref as any}
             className="timeline"
@@ -559,7 +752,12 @@ class Timeline extends React.Component<Props, State> {
             </defs>
             <g transform={'translate(' + margin.left + ',' + margin.top + ')'}>
               <g clipPath="url(#clip)" className="points_g">
-                <Circles points={this.state.points} margin={margin} />
+                <Circles
+                  points={this.state.points}
+                  margin={margin}
+                  onClick={this.props.onClick}
+                  onHover={this.onHover}
+                />
               </g>
               <g
                 className="axis axis--x"
