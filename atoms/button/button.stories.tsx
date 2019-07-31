431989db9c60fbc64bf9cdca976132125d7b4e63
Unknown <blake.thomson@connexta.com>
2019-07-31 09:44:30 -0700

    update paths

--- atoms/button/button.stories.tsx
+++ atoms/button/button.stories.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
-import { storiesOf } from '@storybook/react'
-import { withKnobs, select, boolean } from '@storybook/addon-knobs'
+import { storiesOf } from '@connexta/ace/@storybook/react'
+import { withKnobs, select, boolean } from '@connexta/ace/@storybook/addon-knobs'
 import Button from './button'
 
 storiesOf('Button', module)
