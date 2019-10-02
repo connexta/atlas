508dc89c83b7775baff52f83289df0df7cd9ee46
Unknown <blake.thomson@connexta.com>
2019-10-01 20:39:27 -0700

    Add story and disclaimer for createable select

--- atoms/input/autocomplete.tsx
+++ atoms/input/autocomplete.tsx
@@ -10,6 +10,16 @@ import MenuItem from '@material-ui/core/MenuItem'
 import CancelIcon from '@material-ui/icons/Cancel'
 import { Props as CreatableProps } from 'react-select/src/Creatable'
 
+export type Option = {
+  label: string
+  value: any
+}
+
+export type GroupOptions = {
+  label: string
+  options: Option[]
+}
+
 const useStyles = makeStyles(theme => ({
   input: {
     display: 'flex',
@@ -184,6 +194,16 @@ const components = {
   ValueContainer,
 }
 
+/**
+ * Very important note when using async options: Because `onInputChange` for some reason is an 'interceptor' rather than a handler that ignores
+ * the callback's return value (https://github.com/JedWatson/react-select/issues/1760), if you inline the method like
+ * <CreatableSelect onInputChange={(value) => handleValue(value)}
+ * and handleValue is an asynchronous function, it will attempt to alter the input value with the return value of handleValue, which will
+ * immediately be a promise, causing CreatableSelect to throw an error (in my case `str.replace is not a function`)
+ *
+ * TLDR: You must not use the shorthand arrow syntax to auto return.
+ * <CreatableSelect onInputChange={(value) => {handleValue(value)}} would work without issues.
+ */
 export const WrappedCreatableSelect = (props: CreatableProps<any>) => {
   const classes = useStyles()
   const theme = useTheme()
--- atoms/input/input.stories.tsx
+++ atoms/input/input.stories.tsx
@@ -1,13 +1,36 @@
 import { storiesOf } from '../../storybook'
 import * as React from 'react'
 import { CreatableSelect } from './'
+import { GroupOptions } from './autocomplete'
 
 const stories = storiesOf('Components | Input', module)
 
 stories.add('AutoComplete', () => {
+  const [selectValues, setSelectValues] = React.useState([])
+  const [suggestions, setSuggestions] = React.useState<GroupOptions[]>([])
+
+  console.log('Suggestions: ', suggestions)
+
   return (
     <div style={{ maxWidth: '30%' }}>
-      <CreatableSelect />
+      <CreatableSelect
+        isMulti
+        value={selectValues}
+        onChange={(values: any) => setSelectValues(values)}
+        onInputChange={() => {
+          setSuggestions([
+            {
+              label: 'Group 1',
+              options: [{ label: 'Person 1', value: 'value 1' }],
+            },
+            {
+              label: 'Group 2',
+              options: [{ label: 'Person 2', value: 'value 2' }],
+            },
+          ])
+        }}
+        options={suggestions}
+      />
     </div>
   )
 })
