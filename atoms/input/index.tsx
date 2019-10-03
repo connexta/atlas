8d3bdc1f80a7964e8d9690fac5c7eda5c8dd351c
Blake Thomson <blake.thomson@connexta.com>
2019-10-03 08:18:53 -0700

    Add async creatable select (#32)
    * Add story and disclaimer for createable select

--- atoms/input/index.tsx
+++ atoms/input/index.tsx
@@ -19,4 +19,7 @@ export {
 } from './form-control'
 export { WrappedRadioGroup as RadioGroup, WrappedRadio as Radio } from './radio'
 export { WrappedCheckbox as Checkbox } from './checkbox'
-export { WrappedCreatableSelect as CreatableSelect } from './autocomplete'
+export {
+  WrappedCreatableSelect as CreatableSelect,
+  WrappedAsyncCreatableSelect as AsyncCreatableSelect,
+} from './autocomplete'
