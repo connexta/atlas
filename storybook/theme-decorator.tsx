5dac941199ae5b7e62aa40c60b9fb4c491ccc772
Blake Thomson <blake.thomson@connexta.com>
2019-10-03 08:41:52 -0700

    Add theming to timeline and onCopy function (#31)
    * Add theme to timeline
    
    * Add ability to copy ISO date strings

--- storybook/theme-decorator.tsx
+++ storybook/theme-decorator.tsx
@@ -4,8 +4,37 @@ import * as React from 'react'
 import { ThemeProvider as StyledThemeProvider } from 'styled-components'
 import { Paper } from '../atoms/paper'
 import { ThemeProvider as MuiThemeProvider } from '../atoms/theme'
+import { ThemeColorInterface, ThemeName } from '../styled/theme'
 
 type ColorMode = 'light' | 'dark'
+
+const darkTheme: ThemeColorInterface = {
+  primaryColor: '#32a6ad',
+  positiveColor: '#5b963e',
+  negativeColor: '#943838',
+  warningColor: '#decd39',
+  favoriteColor: '#d1d179',
+  backgroundNavigation: '#213137',
+  backgroundAccentContent: '#34434c',
+  backgroundDropdown: '#253540',
+  backgroundContent: '#253540',
+  backgroundModal: '#253540',
+  backgroundSlideout: '#435059',
+}
+
+const lightTheme: ThemeColorInterface = {
+  primaryColor: '#3c6dd5',
+  positiveColor: '#428442',
+  negativeColor: '#8a423c',
+  warningColor: '#c89600',
+  favoriteColor: '#d1d179',
+  backgroundNavigation: '#3c6dd5',
+  backgroundAccentContent: '#edf9fc',
+  backgroundDropdown: '#f3fdff',
+  backgroundContent: '#f3fdff',
+  backgroundModal: '#edf9fc',
+  backgroundSlideout: '#edf9fc',
+}
 
 const DefaultThemeProvider = (props: {
   children: any
@@ -13,10 +42,19 @@ const DefaultThemeProvider = (props: {
 }) => {
   const { paletteType, children } = props
 
-  const styledTheme = {
-    backgroundContent: paletteType === 'dark' ? '#253540' : '#f3fdff',
-    theme: paletteType,
+  let styledTheme: ThemeColorInterface & ThemeName
+  if (paletteType === 'dark') {
+    styledTheme = {
+      ...darkTheme,
+      theme: paletteType,
+    }
+  } else {
+    styledTheme = {
+      ...lightTheme,
+      theme: paletteType,
+    }
   }
+
   return (
     <StyledThemeProvider theme={styledTheme}>
       <MuiThemeProvider>
